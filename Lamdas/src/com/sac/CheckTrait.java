b3848257bd85528773412991d0dec56be24cd547
srnaik <srnaik@hotmail.com>
2021-02-15 12:17:06 -0800

    Using @FunctionalInterface annotation.

--- Lamdas/src/com/sac/CheckTrait.java
+++ Lamdas/src/com/sac/CheckTrait.java
@@ -1,6 +1,6 @@
 package com.sac;
 
+@FunctionalInterface
 public interface CheckTrait {
     public boolean test(Animal a);
-
 }
--- Lamdas/src/com/sac/TraditionalSearch.java
+++ Lamdas/src/com/sac/TraditionalSearch.java
@@ -12,8 +12,9 @@ public class TraditionalSearch {
         arlAnimals.add(new Animal("rabbit",true,false));
         arlAnimals.add(new Animal("turtle",false,true));
         print(arlAnimals,new CheckIfHopper());
+
         //print(arlAnimals,a->a.isCanHoop());
-        //print(arlAnimals,a->a.isCanSwin());
+        // print(arlAnimals,a->a.isCanSwin());
        // String strName = (String) print(arlAnimals,a->{return (String) a.toString();});
         // System.out.println("Returned String "+ strName);
     }
